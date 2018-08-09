cbe601b6fd9bcaff81c7b8aa85872f5a16dd064e
Machulyanskiy <machulyanskiy.dmytro.wsk@gmail.com>
2018-08-10 01:29:13 +0300

    HOME PAGE add dynamic album description

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,15 @@
 $(function() {
-    /*window.addEventListener('scroll', function(e) {
-        console.log(1);
-        var navbar = document.getElementById('main-nav');
-        if (window.scrollX > 0) {
-            console.log(2);
-            navbar.classList.add('main-nav-active');
-        } else {
-            console.log(3);
-            navbar.classList.remove('main-nav-active');
-        }
-    });*/
-
-   /* $("body").scroll(function(){
-       console.log('bibka');
-
-    });
-*/
-    var offset = $("#container").offset().left - 50;
+    var cont_offset = $("#container").offset().left - 50;
+    var desc_offset = $("#description").offset().left;
     $("body").scroll(function(){
-        if(offset > $("#container").offset().left) {
+        var current = $('#container').offset().left;
+        if(desc_offset > current) {
+            $('#description').addClass('desc-hide');
+        } else {
+            $('#description').removeClass('desc-hide');
+        }
+
+        if(cont_offset > $("#container").offset().left) {
             $('#main-nav').addClass('main-nav-active');
         } else {
             $('#main-nav').removeClass('main-nav-active');
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -33,6 +33,23 @@
             </nav>
         </section>
 
+        <section id="description">
+            <p id="desc-title">Lorem ipsum dolor sit amet</p>
+            {{--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>--}}
+            <div id="desc-text">
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</p>
+                <p>do eiusmod tempor incididunt ut labore et dolore magna</p>
+                <p>aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                <p>ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+                <p>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat</p>
+                <p>cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                <p>mollit anim id est laborum.</p>
+            </div>
+
+
+        </section>
+
         <section id="container">
             @yield('content')
         </section>
